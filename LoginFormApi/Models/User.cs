dcf545eac9772f31ceddf66da631f8bff9ca2ac6
Sanzhar Yerzhanov <sanzar30062000@gmail.com>
2024-11-15 23:15:43 +0500

    add minimalapi(sample)

---
+++ LoginFormApi/Models/User.cs
@@ -0,0 +1,8 @@
+namespace LoginFormApi.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
